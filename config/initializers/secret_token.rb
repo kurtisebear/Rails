38735df1403efefe6b7088ffbd290c5d1a2a99aa
Kurtis Brown <kurtis@kurtisebear.com>
2014-01-02 20:36:39 +0000

    create repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railstut::Application.config.secret_key_base = 'ba1c8a9d4678feea9ea2700dcc43d7d33f381286bf3e3c61bd4c7feddf63301d9086045945a9a7d10ba35e8517e6705ba05d26bd3beaccc8353cdb71fc30a638'
